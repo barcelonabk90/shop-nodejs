5542d82d28b33acfbe858c09d284379242bb29af
QuangBV <buiquangbk90@gmail.com>
2022-03-07 22:48:39 +0900

    1. Fix security problem : db config 2. get a single product 3. update a product

--- models/product.js
+++ models/product.js
@@ -1,20 +1,26 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
-      .then(result => {
-        console.log(result);
-      })
+    let dbObject;
+    if (this._id) {
+      dbObject = db.collection('products').updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbObject = db.collection('products').insertOne(this);
+    }
+    return dbObject.then(result => {
+      console.log(result);
+    })
       .catch(err => {
         console.log(err);
       });
@@ -28,6 +34,19 @@ class Product {
       .toArray()
       .then(products => {
         return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(productId) {
+    const db = getDb();
+    return db.collection('products').find({ _id: new mongodb.ObjectId(productId) })
+      .next()
+      .then(product => {
+        // console.log(product);
+        return product;
       })
       .catch(err => {
         console.log(err);
--- util/database.js
+++ util/database.js
@@ -4,12 +4,14 @@
 
 const mongodb = require('mongodb');
 
+const config = require('../config');
+
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
 const mongoConnect = callback => {
-  MongoClient.connect('mongodb+srv://shopAdmin:jL2D5k45trGS5Lg5@cluster0.1hvb2.mongodb.net/shop10?retryWrites=true&w=majority')
+  MongoClient.connect(`mongodb+srv://${config.db.user}:${config.db.pass}@cluster0.1hvb2.mongodb.net/${config.db.name}?retryWrites=true&w=majority`)
     .then(client => {
       // console.log(client);
       _db = client.db();
